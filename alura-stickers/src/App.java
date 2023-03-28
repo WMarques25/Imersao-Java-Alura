e3e63a4596e2530a49e6ca4dd535d4905e4c8ece
Wellington Pereira Marques <114716548+WMarques25@users.noreply.github.com>
2023-03-28 00:57:08 -0300

    Desafio 1

---
+++ alura-stickers/src/App.java
@@ -0,0 +1,35 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+
+        // fazer uma conexão HTTP e buscar os top 3 S�ries de acordo com IMDB
+        // String url = "https://imdb-api.com/en/API/Top250Movies/ + chave";  (API da imdb com instabilidade, usando arquivo pronto disponibilizado pela Alura)
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopTVs.json";
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+
+        // extrair só os dados que interessam (titulo, poster, classificação)
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+
+        // exibir e manipular os dados 
+        for (int i = 0; i < 3; i++) {
+            Map<String,String> filme = listaDeFilmes.get(i);
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+            System.out.println();
+        }
+    }
+}
